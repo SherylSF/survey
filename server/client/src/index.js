85fbda6866047ebaf62d07025755d6ba05e3ed5d
Sher <siaufui327@hotmail.com>
2024-02-03 16:09:30 +0800

    stripe

---
+++ server/client/src/index.js
@@ -0,0 +1,20 @@
+import 'materialize-css/dist/css/materialize.min.css'
+import React from 'react'
+import REACTDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import {thunk} from 'redux-thunk'
+import App from './components/App'
+import reducers from './reducers'
+
+
+
+const store = createStore(reducers, {}, applyMiddleware(thunk))
+const el = document.getElementById('root')
+
+
+const root = REACTDOM.createRoot(el)
+
+root.render(<Provider store={store}><App/></Provider>)
+
+console.log('STRIPE KEY', process.env.REACT_APP_STRIPE_KEY)
